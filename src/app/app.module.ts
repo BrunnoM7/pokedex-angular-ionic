2dac8989df49d1317f90784e0a32ceab7362a94e
BrunnoM7 <brunno.marchetti7@gmail.com>
2022-02-23 22:39:09 -0300

    project init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,16 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { PokemonNamePipe } from './pipes/pokemon-name.pipe'
+import { PipesModule } from './pipes/pipes.module';
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule, PipesModule
+  ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
---
+++ src/app/pages/pokedex/pokedex.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { PipesModule } from 'src/app/pipes/pipes.module';
+
+import { IonicModule } from '@ionic/angular';
+
+import { PokedexPageRoutingModule } from './pokedex-routing.module';
+
+import { PokedexPage } from './pokedex.page';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    PokedexPageRoutingModule,
+    PipesModule
+  ],
+  declarations: [PokedexPage]
+})
+export class PokedexPageModule {}
